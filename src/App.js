f42e8fea67d0cba9c349d662caabfff213e65d5f
yurchenko08 <yurchenko08@gmail.com>
2022-11-09 20:11:14 +0200

    finished second task

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import './App.css';
-
+import { useEffect, useState } from "react";
+import "./App.css";
 export default function App() {
+  const [value, setValue] = useState(() => {
+    return localStorage.getItem("appData") || " ";
+  });
+  useEffect(() => {
+    localStorage.setItem("appData", value);
+  }, [value]);
 
   return (
     <div>
-      React Marathon, appData: <input size='5'></input>
+      React Marathon, appData:
+      <input
+        size='5'
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        type='number'
+      ></input>
     </div>
   );
 }
